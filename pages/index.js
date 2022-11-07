3635d6b353f8ed346e5d0a4fa656256d59e38932
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-07 10:03:45 +0000

    Add bootstrap carousel

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,66 @@
 import Head from "next/head"
-import { useEffect, useRef } from "react"
-import { CarouselComponent } from "@syncfusion/ej2-react-navigations"
 import PropgressNav from "../components/PropgressNav"
 import styles from "../styles/Home.module.scss"
-
+import Carousel from "react-bootstrap/Carousel"
 export default function Home() {
     return (
         <>
             <PropgressNav />
             <div className={styles.container}>
-                <div className={styles.videoText}>
+                {/* <div className={styles.videoText}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sint libero eaque incidunt officiis modi!
-                </div>
-                <video autoPlay loop muted>
-                    <source
-                        src="https://www.accenture.com/t00010101T000000Z__w__/gb-en/_acnmedia/Thought-Leadership-Assets/Videos/Accenture-Maturity-Index-Marquee-Video-1920x1080.mp4"
-                        type="video/mp4"
-                    />
-                </video>
-                <video autoPlay loop muted>
-                    <source
-                        src="https://www.accenture.com/t00010101T000000Z__w__/gb-en/_acnmedia/Thought-Leadership-Assets/Videos-2/Accenture-Tech-Vision-2022-Marquee.mp4%22%20type=%22video/mp4"
-                        type="video/mp4"
-                    />
-                </video>
+                </div> */}
+                <Carousel interval={2500}>
+                    <Carousel.Item>
+                        <video autoPlay loop muted>
+                            <source
+                                src="https://www.accenture.com/t00010101T000000Z__w__/gb-en/_acnmedia/Thought-Leadership-Assets/Videos/Accenture-Maturity-Index-Marquee-Video-1920x1080.mp4"
+                                type="video/mp4"
+                            />
+                        </video>
+                        <Carousel.Caption className={styles.videoText}>
+                            <h1>The art of AI maturity</h1>
+                            <p>Advancing from practice to performance</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <video autoPlay loop muted>
+                            <source
+                                src="https://www.accenture.com/t00010101T000000Z__w__/gb-en/_acnmedia/Thought-Leadership-Assets/Videos-2/Accenture-Tech-Vision-2022-Marquee.mp4%22%20type=%22video/mp4"
+                                type="video/mp4"
+                            />
+                        </video>
+                        <Carousel.Caption className={styles.videoText}>
+                            <h1>Technology Vision 2022</h1>
+                            <p>Meet me in the metaverse</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <video autoPlay loop muted>
+                            <source
+                                src="https://www.accenture.com/t00010101T000000Z__w__/gb-en/_acnmedia/Thought-Leadership-Assets/Videos-2/Accenture-CFEEA-Campaign-video.mp4%22%20type=%22video/mp4"
+                                type="video/mp4"
+                            />
+                        </video>
+                        <Carousel.Caption className={styles.videoText}>
+                            <h1>When you embrace change,you can make a world of difference</h1>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <video autoPlay loop muted>
+                            <source
+                                src="https://www.accenture.com/t00010101T000000Z__w__/gb-en/_acnmedia/Thought-Leadership-Assets/Videos-2/Accenture-IndustryX-Marquee-video-1080x1920.mp4%22%20type=%22video/mp4"
+                                type="video/mp4"
+                            />
+                        </video>
+                        <Carousel.Caption className={styles.videoText}>
+                            <h1>Digitise what you make. Revolutionise how you make it.</h1>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
                 <h1>My page</h1>
             </div>
-
         </>
     )
 }
