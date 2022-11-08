834f59cea3c0b7021d6813c290cfb9af9f2d2c32
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-11-08 18:00:36 +0000

    Add css transition

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from "react"
+import { CSSTransition } from "react-transition-group"
+import StylesNav from "../styles/Navigate.module.scss"
 
 const NavMenu = (props) => {
     const [open, setOpen] = useState(false)
@@ -14,13 +16,18 @@ const NavMenu = (props) => {
             document.body.removeEventListener("click", clickOutside)
         }
     }, [])
-    
-    const changeVisible=()=>{
+
+    const changeVisible = () => {
         setOpen(!open)
     }
-  return (
-    <div ref={sortRef} ><p onClick={changeVisible}>{props.title}</p>{open && props.children}</div>
-  )
+    return (
+        <div ref={sortRef} className={StylesNav.navMenu} >
+            <p onClick={changeVisible} className={`${open?StylesNav.active:''}`}>{props.title}</p>
+            <CSSTransition in={open} classNames="alert1" timeout={400} unmountOnExit>
+                {props.children}
+            </CSSTransition>
+        </div>
+    )
 }
 
 export default NavMenu
--- components/navElements/Insights.js
+++ components/navElements/Insights.js
@@ -1,9 +1,44 @@
+import Link from "next/link"
 import StylesNav from "../../styles/Navigate.module.scss"
 
 const Insights = () => {
-  return (
-    <div className={StylesNav.insights}>Insights</div>
-  )
+    return (
+        <div className={StylesNav.insights}>
+          <div className={StylesNav.secondaryNav}>
+            <div className={StylesNav.leftBlock}>
+                <p className={StylesNav.boldP}>FEATURED CONTENT</p>
+                <Link href="/" className={StylesNav.link}>
+                    Voices of Change
+                </Link>
+                <p>
+                    The path to 360° value starts here-featuring our most provocative thinking, extensive research and compelling
+                    stories of shared success.
+                </p>
+                <Link href="/" className={StylesNav.link}>
+                    War in Ukraine
+                </Link>
+                <p>Addressing the business and economic impact.</p>
+            </div>
+            <div className={StylesNav.rightBlock}>
+                <Link href="/">5G</Link>
+                <Link href="/">Artificial Intelligence</Link>
+                <Link href="/">Blockchain</Link>
+                <Link href="/">Cloud</Link>
+
+                <Link href="/">Customer Experience</Link>
+                <Link href="/">Cybersecurity</Link>
+                <Link href="/">Digital Engineering & <br /> Manufacturing </Link>
+                <Link href="/">Digital Transformation</Link>
+                <Link href="/">Edge Computing</Link>
+                <Link href="/">Future of Work</Link>
+                <Link href="/">Supply Chain</Link>
+                <Link href="/">Sustainability</Link>
+                <Link href="/">Podcasts</Link>
+                <Link href="/">Blogs</Link>
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default Insights
