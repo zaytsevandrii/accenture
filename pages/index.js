89d0783feb83e6e9216b7f9923a1fa4b09352d1d
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-02-06 15:44:51 +0000

    loop videos

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
             <div className={styles.container}>
                 <Carousel interval={3900}>
                     <Carousel.Item>
-                        <video autoPlay loop muted>
+                        <video autoPlay loop muted playsinline>
                             <source
                                 src="https://firebasestorage.googleapis.com/v0/b/dimax-f1bfc.appspot.com/o/Maturity-Index-Marquee-Video-1920x1080.mp4?alt=media&token=e0fee0e3-a01f-4213-880a-9789d3824e1f"
                                 type="video/mp4"
@@ -22,7 +22,7 @@ export default function Home() {
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <video autoPlay loop muted>
+                        <video autoPlay loop muted playsinline>
                             <source
                                 src="https://firebasestorage.googleapis.com/v0/b/dimax-f1bfc.appspot.com/o/Tech-Vision-2022-Marquee.mp4?alt=media&token=69b1bc11-a1c0-4113-8ef8-4b2d6ddfa3ec"
                                 type="video/mp4"
@@ -35,7 +35,7 @@ export default function Home() {
                     </Carousel.Item>
 
                     <Carousel.Item>
-                        <video autoPlay loop muted>
+                        <video autoPlay loop muted playsinline>
                             <source
                                 src="https://firebasestorage.googleapis.com/v0/b/dimax-f1bfc.appspot.com/o/CFEEA-Campaign-video.mp4?alt=media&token=b79f7a94-78f3-4971-9c55-a19bc0ec407f"
                                 type="video/mp4"
@@ -46,7 +46,7 @@ export default function Home() {
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <video autoPlay loop muted>
+                        <video autoPlay loop muted playsinline>
                             <source
                                 src="https://firebasestorage.googleapis.com/v0/b/dimax-f1bfc.appspot.com/o/IndustryX-Marquee-video-1080x1920.mp4?alt=media&token=41d638fb-60b4-4c37-bf53-e9c1bad18bc7"
                                 type="video/mp4"
